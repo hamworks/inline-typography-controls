7138749699aaf05b83ffe165a2beac41a05e5554
Hiroshi Urabe <mail@torounit.com>
2025-01-31 04:29:24 +0900

    add control

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,123 @@
+import { type RefObject, useState } from 'react';
 import {
 	registerFormatType,
-	applyFormat,
 	removeFormat,
+	applyFormat,
+	useAnchor,
+	getActiveFormat,
 } from '@wordpress/rich-text';
-import { RichTextToolbarButton } from '@wordpress/block-editor';
+import {
+	RichTextToolbarButton,
+	// @ts-ignore
+	useSettings,
+} from '@wordpress/block-editor';
+import { Popover, FontSizePicker, Button } from '@wordpress/components';
+import type { RichTextValue } from '@wordpress/rich-text/build-types/remove-format';
+import { __ } from '@wordpress/i18n';
 
 const formatName = 'inline-font-control/sample-output';
 
 // @ts-ignore
-const MyCustomButton = ( { isActive, onChange, value } ) => {
+const Edit = ( { isActive, onChange, value, contentRef } ) => {
+	const [ isPopoverVisible, setIsPopoverVisible ] = useState( false );
+	const togglePopover = () => {
+		setIsPopoverVisible( ( state ) => ! state );
+	};
 	return (
-		<RichTextToolbarButton
-			icon="editor-code"
-			title="Sample output"
-			onClick={ () => {
-				if ( isActive ) {
-					onChange( removeFormat( value, formatName ) );
-				} else {
-					onChange(
-						applyFormat( value, {
-							type: formatName,
-						} )
-					);
-				}
-			} }
-			isActive={ isActive }
-		/>
+		<>
+			<RichTextToolbarButton
+				icon="editor-code"
+				title="Sample output"
+				onClick={ () => {
+					if ( isActive ) {
+						onChange( removeFormat( value, formatName ) );
+					} else {
+						togglePopover();
+					}
+				} }
+				isActive={ isActive }
+				role="menuitemcheckbox"
+			/>
+			{ isPopoverVisible && (
+				<InlineUI
+					isActive={ isActive }
+					value={ value }
+					onChange={ onChange }
+					onClose={ togglePopover }
+					contentRef={ contentRef }
+				/>
+			) }
+		</>
 	);
 };
 
-registerFormatType( formatName, {
+type InlineUIProps = {
+	value: RichTextValue;
+	contentRef: RefObject< HTMLElement >;
+	onChange: ( value: any ) => void;
+	onClose: () => void;
+	isActive: boolean;
+};
+function InlineUI( {
+	value,
+	contentRef,
+	onChange,
+	onClose,
+	isActive,
+}: InlineUIProps ) {
+	const [ fontSizes ] = useSettings( 'typography.fontSizes' );
+	const activeInlineFontSizeFormat = getActiveFormat( value, formatName );
+	// @ts-ignore
+	const activeFontSize: string = activeInlineFontSizeFormat?.attributes?.data;
+	const popoverAnchor = useAnchor( {
+		editableContentElement: contentRef.current,
+		// @ts-ignore
+		settings: { ...settings, isActive },
+	} );
+
+	const onChangeFontSize = ( fontSize: string | number | undefined ) => {
+		if ( ! fontSize ) {
+			onChange( removeFormat( value, formatName ) );
+		} else {
+			// @ts-ignore
+			onChange(
+				applyFormat( value, {
+					type: formatName,
+					// @ts-ignore
+					attributes: {
+						data: `${ fontSize }`,
+						style: `font-size: ${ fontSize };`,
+					},
+				} )
+			);
+		}
+	};
+
+	return (
+		<Popover anchor={ popoverAnchor } onClose={ onClose }>
+			<div style={ { padding: '1em', width: '300px' } }>
+				<FontSizePicker
+					fontSizes={ fontSizes }
+					onChange={ onChangeFontSize }
+					value={ activeFontSize }
+					withSlider
+					size="__unstable-large"
+				/>
+				<Button onClick={ onClose } variant="secondary">
+					{ __( 'Apply' ) }
+				</Button>
+			</div>
+		</Popover>
+	);
+}
+
+const settings = {
 	title: 'Sample output',
 	tagName: 'span',
 	className: 'has-inline-font-size',
 	interactive: false,
 	name: formatName,
-	edit: MyCustomButton,
-} );
+	edit: Edit,
+};
+
+registerFormatType( formatName, settings );
