03285522ff9240a52b6b1488361ddf0f42518041
Hiroshi Urabe <mail@torounit.com>
2025-02-02 16:36:04 +0900

    add inline block

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
-import * as fontSize from './fontSize';
 import { registerFormatType } from '@wordpress/rich-text';
 
+import * as fontSize from './fontSize';
+import * as inlineBlock from './inlineBlock';
+
 registerFormatType( fontSize.formatName, fontSize.settings );
+registerFormatType( inlineBlock.formatName, inlineBlock.settings );
---
+++ src/inlineBlock/index.tsx
@@ -0,0 +1,32 @@
+import { __ } from '@wordpress/i18n';
+import { toggleFormat } from '@wordpress/rich-text';
+import { RichTextToolbarButton } from '@wordpress/block-editor';
+import { square } from '@wordpress/icons';
+
+export const formatName = 'inline-typography-controls/inline-block';
+
+export const settings = {
+	title: __( 'Inline Block' ),
+	tagName: 'span',
+	className: 'has-inline-block',
+	interactive: false,
+	name: formatName,
+	edit: Edit,
+};
+
+// @ts-ignore
+function Edit( { isActive, onChange, value } ) {
+	return (
+		<>
+			<RichTextToolbarButton
+				icon={ square }
+				title={ settings.title }
+				onClick={ () =>
+					onChange( toggleFormat( value, { type: formatName } ) )
+				}
+				isActive={ isActive }
+				role="menuitemcheckbox"
+			/>
+		</>
+	);
+}
